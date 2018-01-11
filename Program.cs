6b765f264f14a32ef4566296800f767f88e823cf
rkdk12 <kjg@mz.co.kr>
2018-01-11 18:12:22 +0900

    init heroku

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace signalr_aspnetcore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
+        public static IWebHost BuildWebHost(string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .UseUrls("http://corechat.com:9999")
+        .UseIISIntegration()
+        //.UseKestrel(options =>
+        //{
+        //    options.Listen(IPAddress.Loopback, 5000);
+
+        //}
+        .UseKestrel()
+        .Build();
+    }
+}
