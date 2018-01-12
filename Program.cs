a0bd39cfd841b7f541fabea9ea70295a2474c679
rkdk12 <kjg@mz.co.kr>
2018-01-12 11:51:49 +0900

    make it better233

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace signalr_aspnetcore
         public static IWebHost BuildWebHost(string[] args) =>
     WebHost.CreateDefaultBuilder(args)
         .UseStartup<Startup>()
-        .UseUrls("http://corechat.com:9999")
+        .UseUrls("http://https://ancient-shelf-70200.herokuapp.com/")
         .UseIISIntegration()
         //.UseKestrel(options =>
         //{
