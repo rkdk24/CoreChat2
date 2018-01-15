fe77f96fe608fad83dfddb2d9f599618d0eb4cfd
rkdk12 <kjg@mz.co.kr>
2018-01-15 17:07:19 +0900

    make it beaaaasdgafdg

--- ChatHub.cs
+++ ChatHub.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,17 +8,23 @@ namespace signalr_aspnetcore
     {
         public override Task OnConnectedAsync()
         {
+            Console.WriteLine("OnConnectedAsync");
+
             Clients.All.InvokeAsync("broadcastMessage", "system", $"{Context.User.Identity.Name} joined the conversation");
             return base.OnConnectedAsync();
         }
         public void Send(string name, string message)
         {
+            Console.WriteLine("send");
+
             // Call the broadcastMessage method to update clients.
             Clients.All.InvokeAsync("broadcastMessage", name, message);
         }
 
         public override Task OnDisconnectedAsync(System.Exception exception)
         {
+            Console.WriteLine("OnDisconnectedAsync");
+
             Clients.All.InvokeAsync("broadcastMessage", "system", $"{Context.ConnectionId} left the conversation");
             return base.OnDisconnectedAsync(exception);
         }
