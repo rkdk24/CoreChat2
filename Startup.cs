c37ea007f5bf881f882f77533cd02f05443e4ce0
rkdk12 <kjg@mz.co.kr>
2018-01-16 11:55:11 +0900

    make it beaaaasdgafdg

--- Startup.cs
+++ Startup.cs
@@ -15,12 +15,14 @@ namespace signalr_aspnetcore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine("ConfigureServices");
             services.AddSignalR();
         }
        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Console.WriteLine("Configure");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
